6451ff3de4288987779ca8b45a329a18170c8ee2
Iván Pérez Cóyotl <iperez.c@outlook.com>
2021-01-24 23:46:53 -0600

    Update EmailComp.java

--- src/main/java/com/ivoslabs/mail/comp/EmailComp.java
+++ src/main/java/com/ivoslabs/mail/comp/EmailComp.java
@@ -3,8 +3,10 @@
  */
 package com.ivoslabs.mail.comp;
 
+import java.io.File;
+
 /**
- * @since
+ * @since 1.0.0
  * @author www.ivoslabs.com
  *
  */
@@ -12,4 +14,55 @@ public interface EmailComp {
 
     boolean sendEmail(String to, String subject, String text);
 
+    boolean sendEmail(String[] to, String subject, String text);
+
+    //
+
+    boolean sendEmail(String to, String cc, String subject, String text);
+
+    boolean sendEmail(String to, String[] cc, String subject, String text);
+
+    boolean sendEmail(String[] to, String cc, String subject, String text);
+
+    boolean sendEmail(String[] to, String[] cc, String subject, String text);
+
+    boolean sendEmail(String[] to, String[] cc, String[] bc, String subject, String text);
+
+    //
+
+    boolean sendEmail(Enum<?> distList, String subject, String text);
+
+    boolean sendEmail(Enum<?> distList, Enum<?> distListCC, String subject, String text);
+
+    boolean sendEmail(Enum<?> distList, Enum<?> distListCC, Enum<?> distListBC, String subject, String text);
+
+    //
+    //
+    // attachment
+    //
+    //
+
+    boolean sendEmail(String to, String subject, String text, File attachment);
+
+    boolean sendEmail(String[] to, String subject, String text, File attachment);
+
+    //
+
+    boolean sendEmail(String to, String cc, String subject, String text, File attachment);
+
+    boolean sendEmail(String to, String[] cc, String subject, String text, File attachment);
+
+    boolean sendEmail(String[] to, String cc, String subject, String text, File attachment);
+
+    boolean sendEmail(String[] to, String[] cc, String subject, String text, File attachment);
+
+    boolean sendEmail(String[] to, String[] cc, String[] bc, String subject, String text, File attachment);
+
+    //
+
+    boolean sendEmail(Enum<?> distList, String subject, String text, File attachment);
+
+    boolean sendEmail(Enum<?> distList, Enum<?> distListCC, String subject, String text, File attachment);
+
+    boolean sendEmail(Enum<?> distList, Enum<?> distListCC, Enum<?> distListBC, String subject, String text, File attachment);
 }
