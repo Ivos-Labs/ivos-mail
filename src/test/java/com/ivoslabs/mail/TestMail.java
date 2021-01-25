91519cc165de21de9b7f6a6739150df9a38507d5
Iván Pérez Cóyotl <iperez.c@outlook.com>
2021-01-24 23:47:05 -0600

    Update TestMail.java

--- src/test/java/com/ivoslabs/mail/TestMail.java
+++ src/test/java/com/ivoslabs/mail/TestMail.java
@@ -12,9 +12,12 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import com.ivoslabs.mail.comp.EmailComp;
+import com.ivoslabs.mail.enums.ExampleLists;
 
 /**
- * @since
+ * Test
+ *
+ * @since 1.0.0
  * @author www.ivoslabs.com
  *
  */
@@ -22,15 +25,39 @@ import com.ivoslabs.mail.comp.EmailComp;
 @ContextConfiguration("classpath:spring/application-context.xml")
 public class TestMail {
 
+
+    /** Email component */
     @Autowired
     private EmailComp emailComp;
 
     @Test
     public void test01() {
+        assertTrue(this.emailComp.sendEmail("user01@gmail.com", "Test-01", "<b>Content</b>"));
+    }
 
-        this.emailComp.sendEmail("iperez.c@outlook.com", "test02", "<b>texto02</b>");
+    @Test
+    public void test02() {
+        assertTrue(this.emailComp.sendEmail("user01@gmail.com", "user02@gmail.com", "Test-01", "<b>Content</b>"));
+    }
 
-        assertTrue(Boolean.TRUE);
+    @Test
+    public void test03() {
+        assertTrue(this.emailComp.sendEmail(new String[] { "user01@gmail.com" }, new String[] { "user02@gmail.com" }, new String[] { "user03@gmail.com" }, "Test-01", "<b>Content</b>"));
+    }
+
+    @Test
+    public void testEnum01() {
+        assertTrue(this.emailComp.sendEmail(ExampleLists.ADMIN, "Test-01", "<b>Content</b>"));
+    }
+
+    @Test
+    public void testEnum02() {
+        assertTrue(this.emailComp.sendEmail(ExampleLists.ADMIN, ExampleLists.SUPPORT, "Test-01", "<b>Content</b>"));
+    }
+
+    @Test
+    public void testEnum03() {
+        assertTrue(this.emailComp.sendEmail(ExampleLists.ADMIN, ExampleLists.SUPPORT, ExampleLists.OTHER_DIST_LIST, "Test-01", "<b>Content</b>"));
     }
 
 }
