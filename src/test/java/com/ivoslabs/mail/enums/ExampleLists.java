8d84b94d9c28db94008a808c924627bbd2c1c723
Iván Pérez Cóyotl <iperez.c@outlook.com>
2021-01-24 23:47:02 -0600

    Create ExampleLists.java

---
+++ src/test/java/com/ivoslabs/mail/enums/ExampleLists.java
@@ -0,0 +1,23 @@
+/**
+ *
+ */
+package com.ivoslabs.mail.enums;
+
+/**
+ * Enum of example of lists of recipients
+ *
+ * @since 1.0.0
+ * @author www.ivoslabs.com
+ *
+ */
+public enum ExampleLists {
+
+    ADMIN(),
+
+    SUPPORT(),
+
+    OTHER_DIST_LIST(),
+
+    ;
+
+}
